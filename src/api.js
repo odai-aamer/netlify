008b867c3199f636f292aee48041c654d48e8381
odaiamer-97 <odaiamer97@gmail.com>
2023-05-15 20:22:27 +0400

    netlify express

---
+++ src/api.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const productController = require('./controllers/product');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+router.get("/", (req, res) => {
+  res.json({
+    message: "Home Page"
+  });
+});
+
+// GET /products
+router.get('/products', productController.getProducts);
+
+// GET /creative-products
+router.get('/creative-products', productController.getProducts);
+
+
+// POST /product
+router.post('/product', productController.createProduct);
+
+// POST /creative-product
+router.post('/creative-product', productController.createCreativeProduct);
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
